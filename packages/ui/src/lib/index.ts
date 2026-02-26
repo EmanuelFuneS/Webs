81e640ec98d4516d6e83b4e1497929b56e1fc4b8
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-02-26 04:53:00 +0000

    fix: exports

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,2 +1,6 @@
-export * from "react-icons";
+export * from "react-icons/ai";
+export * from "react-icons/bs";
+export * from "react-icons/fa";
 export { FaCheckCircle } from "react-icons/fa";
+export * from "react-icons/io5";
+export * from "react-icons/ri";
